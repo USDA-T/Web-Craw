dda2477ef6750a4492f68c997b911eb64eea4860
Deric <deric.nguni@gmail.com>
2016-08-08 16:04:59 -0400

    Us1265 uswds sole pand corporation (#45)
    * partial continues commit for Us1265
    
    * Minor change

--- src/test/java/gov/sba/utils/helpers/LoginHelpers.java
+++ src/test/java/gov/sba/utils/helpers/LoginHelpers.java
@@ -18,7 +18,7 @@ public class LoginHelpers {
 
 	public static LoginData getLoginData() throws Exception {
 		List<LoginData> loginFixtures = LoginHelpers.loadFixtures();
-		return loginFixtures.get(2);
+		return loginFixtures.get(6);
 	}
 
 	public static List<LoginData> loadFixtures() throws Exception {
