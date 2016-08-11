012fe4afcc15d93e5da1bcf83d35d4578aaaa014
Deric <deric.nguni@gmail.com>
2016-08-11 08:22:45 -0400

    updates for testsearchpage (#55)

--- src/test/java/gov/sba/utils/TestSearchPage.java
+++ src/test/java/gov/sba/utils/TestSearchPage.java
@@ -45,15 +45,12 @@ public class TestSearchPage extends TestCase {
 	public void testMainLogic() throws Exception {
 		logger.debug("Using test login   : " + LoginHelpers.getLoginData().getEmail());
 		logger.debug("Using test password: " + LoginHelpers.getLoginData().getPassword());
-
-		webDriver.findElement(By.xpath(".//*[@id=\"gov_login_box\"]/form[1]/button")).click();
-
+		webDriver.findElement(By.cssSelector("button.button-full")).click();
 		webDriver.findElement(By.name("user[email]")).sendKeys(LoginHelpers.getLoginData().getEmail());
 		webDriver.findElement(By.name("user[password]")).sendKeys(LoginHelpers.getLoginData().getPassword());
-
 		webDriver.findElement(By.id("business_signin")).click();
 		String url = webDriver.getCurrentUrl();
-		assertTrue(url.contains("dashboard"));
+		org.junit.Assert.assertTrue(url.contains("dashboard"));
 	}
 
 }
