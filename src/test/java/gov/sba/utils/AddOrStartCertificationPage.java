7706208b01bf93f4ac2476e3e4cdf126c4e1b539
Deric <deric.nguni@gmail.com>
2016-10-13 10:08:28 -0400

    doc upload changes (#105)

--- src/test/java/gov/sba/utils/AddOrStartCertificationPage.java
+++ src/test/java/gov/sba/utils/AddOrStartCertificationPage.java
@@ -23,10 +23,10 @@ public class AddOrStartCertificationPage {
 			// Click on the certification link.
 			webDriver.findElement(By.linkText("Programs")).click();
 			Actual_Text = webDriver.findElement(By.xpath("//h1[2]")).getText();
-			Expected_Text = "Start a new certification";
+			Expected_Text = "Join a new program";
 			assertEquals(Actual_Text, Expected_Text);
 			// Verify and Click on the EDWOSB Certification to select.
-			Actual_Text = webDriver.findElement(By.xpath("//div[@id='certificate_choice']/label")).getText();
+			Actual_Text = webDriver.findElement(By.xpath("//div[@id='certificate_choice']/fieldset/label[2]")).getText();
 			Expected_Text = "Economically Disadvantaged Woman Owned Small Business (EDWOSB) Program (Review requirements)";
 			assertEquals(Actual_Text, Expected_Text);
 			webDriver.findElement(By.id("certificate_type_edwosb")).click();
@@ -48,7 +48,7 @@ public class AddOrStartCertificationPage {
 			webDriver.findElement(By.linkText("EDWOSB self-certification")).click();
 			// Verify and validate the terms for the certification.
 			Thread.sleep(4000);
-			Actual_Text = webDriver.findElement(By.xpath("//form[@id='new_sba_application']/div[2]")).getText();
+			Actual_Text = webDriver.findElement(By.xpath("//form/div[2]")).getText();
 			Expected_Text = "All small businesses that are interested in submitting an offer on a solicitation that has been set aside for Women-Owned Small Businesses (WOSB) and Economically Disadvantaged Women-Owned Small Businesses (EDWOSB) under the WOSB Program must complete this certification prior to submitting the offer. This includes checking all boxes presented and having an authorized officer of the woman-owned small business or economically disadvantaged woman-owned small business electronically sign and date the certification. You must upload all other required documents as indicated here-in to the WOSB Program Repository. For questions, please e-mail wosb@sba.gov.\nPlease read the following certification statements. The Federal government relies on the information in this form and any documents or supplemental information submitted to determine whether the business is eligible for a contract authorized under the WOSB Program. The definitions for the terms used in this certification are set forth in the Small Business Act, U.S. Small Business Administration (SBA) regulations (13 C.F.R. Part 127), and also any statutory and regulatory provision referenced in those authorities. In addition, please note that the SBA may request further clarification or supporting documentation in order to assist in the verification of any of the information provided and that each person signing this certification may be prosecuted if they have provided false information. Any action taken with respect to this certification does not affect the Government's right to pursue criminal, civil or administrative remedies for incorrect or incomplete information given, even if correct information has been included in other materials submitted to SBA.";
 			assertEquals(Actual_Text, Expected_Text);
 		}
