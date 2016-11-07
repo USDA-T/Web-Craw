7b2a7134a0fe75c23972b986bc4657188601bebb
Deric <deric.nguni@gmail.com>
2016-11-07 10:34:45 -0500

    commit for us1609 (#120)

--- src/test/java/gov/sba/utils/ScreenShotPage3.java
+++ src/test/java/gov/sba/utils/ScreenShotPage3.java
@@ -2,6 +2,7 @@ package gov.sba.utils;
 
 import java.io.File;
 import java.io.IOException;
+
 import org.apache.commons.io.FileUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -9,23 +10,24 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
-public class ScreenShotPage {
-	private static final Logger logger = LogManager.getLogger(ScreenShotPage.class.getName());
+public class ScreenShotPage3 {
+	private static final Logger logger = LogManager.getLogger(ScreenShotPage3.class.getName());
 	WebDriver webDriver;
 
-	public ScreenShotPage(WebDriver webDriver) {
+	public ScreenShotPage3(WebDriver webDriver) {
 		this.webDriver = webDriver;
 	}
 
-	public void ScreenShot() throws Exception {
+	public void ScreenShot3() throws Exception {
 		// Take screenshot and store as a file format
 		File src = ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.FILE);
 		try {
 			// now copy the screenshot to the screenshot folder.
-			FileUtils.copyFile(src, new File("./Screenshots/ReadOnlyCert.png"));
+			FileUtils.copyFile(src, new File("./Screenshots/Versions.png"));
 		} catch (IOException e) {
 			logger.info(e.getMessage());
 
 		}
 	}
 }
+
