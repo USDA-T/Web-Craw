b6c644dc9da567181b029f09969b9cd264bcf78a
Deric <deric.nguni@gmail.com>
2016-11-21 13:38:00 -0500

    clean up (#140)

--- src/test/java/gov/sba/utils/WorkflowPages/fillApplCreatePages.java
+++ src/test/java/gov/sba/utils/WorkflowPages/fillApplCreatePages.java
@@ -5,8 +5,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import java.util.List;
 
 public class fillApplCreatePages {
 	private static final Logger logger = LogManager.getLogger(fillApplCreatePages.class.getName());
