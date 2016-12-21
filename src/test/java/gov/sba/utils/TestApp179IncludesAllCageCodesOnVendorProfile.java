82d5e556c4ecf941f28b19c2a506d09b69c48fd2
Deric <deric.nguni@gmail.com>
2016-12-21 11:22:29 -0500

    adding capability to connect to business on test account with no roles (#160)

--- src/test/java/gov/sba/utils/TestApp179IncludesAllCageCodesOnVendorProfile.java
+++ src/test/java/gov/sba/utils/TestApp179IncludesAllCageCodesOnVendorProfile.java
@@ -21,7 +21,7 @@ import junit.framework.TestCase;
 			webDriver = TestHelpers.getDefaultWebDriver();
 			webDriver.get(TestHelpers.getBaseUrl());
 			webDriver.manage().window().maximize();
-			get_The_Row_From_Login_Data = 25;
+			get_The_Row_From_Login_Data = 37;
 
 		}
 		
@@ -34,6 +34,102 @@ import junit.framework.TestCase;
 			LoginPageWithReference login_Data = new LoginPageWithReference(webDriver, get_The_Row_From_Login_Data);
 			login_Data.Login_With_Reference();
 			Thread.sleep(3000);
+			//Verify if user has claim a business if not claim one.
+			if (webDriver.getPageSource().contains("Request access")) {
+				Actual_Text = webDriver.findElement(By.cssSelector("h2")).getText();
+				Expected_Text = "Request access";
+				assertEquals(Actual_Text, Expected_Text);
+				webDriver.findElement(By.id("request_new_application")).click();
+				webDriver.findElement(By.name("commit")).click();
+				webDriver.findElement(By.id("search_duns_number")).sendKeys("791985859");
+				webDriver.findElement(By.id("search_ssn_ein")).sendKeys("123456789");
+				webDriver.findElement(By.id("search_mpin")).sendKeys("A12345678");
+				webDriver.findElement(By.xpath("//button")).click();
+				webDriver.findElement(By.id("business_type")).click();
+				webDriver.findElement(By.xpath("//option[4]")).click();
+				webDriver.findElement(By.id("connect")).click();
+				Actual_Text = webDriver.findElement(By.cssSelector("h2 > a")).getText();
+				Expected_Text = "My business";
+				assertEquals(Expected_Text, Actual_Text);
+				Actual_Text = webDriver.findElement(By.xpath("//p[3]/b")).getText();
+				Expected_Text = "CAGE:";
+				assertEquals(Actual_Text, Expected_Text);
+				Actual_Text = webDriver.findElement(By.xpath("//p[3]/span")).getText();
+				Expected_Text = "Z495F, K70WB";
+				assertEquals(Expected_Text, Actual_Text);
+				WebElement CAGECodes = webDriver.findElement(By.xpath("//p[3]/span"));
+				HighLight.highLightElement(webDriver, CAGECodes);
+			    //Logout and login as an analyst.
+				webDriver.findElement(By.linkText("Logout")).click();
+				get_The_Row_From_Login_Data = 0;
+				LoginPageWithReference login_Data1 = new LoginPageWithReference(webDriver, get_The_Row_From_Login_Data);
+				login_Data1.Login_With_Reference();
+				//Search the business and verify the CAGE Codes.
+				webDriver.findElement(By.id("query")).sendKeys("791985859");
+				webDriver.findElement(By.xpath("//button")).click();
+				webDriver.findElement(By.linkText("Entity 90 Legal Business Name")).click();
+				Actual_Text = webDriver.findElement(By.xpath("//p[4]/b")).getText();
+				Expected_Text = "CAGE:";
+				assertEquals(Expected_Text, Actual_Text);
+				Actual_Text = webDriver.findElement(By.xpath("//div[@id='business_search']/div/div/div/div/p[4]/span")).getText();
+				Expected_Text = "Z495F, K70WB";
+				assertEquals(Expected_Text, Actual_Text);
+				WebElement CAGECodesAP = webDriver.findElement(By.xpath("//div[@id='business_search']/div/div/div/div/p[4]/span"));
+				HighLight.highLightElement(webDriver, CAGECodesAP);
+				//Logout and login as an 8(a)analyst.
+				webDriver.findElement(By.linkText("Logout")).click();
+				get_The_Row_From_Login_Data = 34;
+				LoginPageWithReference login_Data2 = new LoginPageWithReference(webDriver, get_The_Row_From_Login_Data);
+				login_Data2.Login_With_Reference();
+				//Search the business and verify the CAGE Codes.
+				webDriver.findElement(By.id("query")).sendKeys("791985859");
+				webDriver.findElement(By.xpath("//button")).click();
+				webDriver.findElement(By.linkText("Entity 90 Legal Business Name")).click();
+				Actual_Text = webDriver.findElement(By.xpath("//p[4]/b")).getText();
+				Expected_Text = "CAGE:";
+				assertEquals(Expected_Text, Actual_Text);
+				Actual_Text = webDriver.findElement(By.xpath("//div[@id='business_search']/div/div/div/div/p[4]/span")).getText();
+				Expected_Text = "Z495F, K70WB";
+				assertEquals(Expected_Text, Actual_Text);
+				WebElement CAGECodes8aA = webDriver.findElement(By.xpath("//div[@id='business_search']/div/div/div/div/p[4]/span"));
+				HighLight.highLightElement(webDriver, CAGECodes8aA);
+				//Logout and login as an OPS support.
+				webDriver.findElement(By.linkText("Logout")).click();
+				get_The_Row_From_Login_Data = 28;
+				LoginPageWithReference login_Data3 = new LoginPageWithReference(webDriver, get_The_Row_From_Login_Data);
+				login_Data3.Login_With_Reference();
+				//Search the business and verify the CAGE Codes.
+				webDriver.findElement(By.id("query")).sendKeys("791985859");
+				webDriver.findElement(By.xpath("//button")).click();
+				webDriver.findElement(By.linkText("Entity 90 Legal Business Name")).click();
+				Actual_Text = webDriver.findElement(By.xpath("//p[4]/b")).getText();
+				Expected_Text = "CAGE:";
+				assertEquals(Actual_Text, Expected_Text);
+				Actual_Text = webDriver.findElement(By.xpath("//div[@id='business_search']/div/div/div/div/p[4]/span")).getText();
+				Expected_Text = "Z495F, K70WB";
+				assertEquals(Actual_Text, Expected_Text);
+				WebElement CAGECodesOpsS = webDriver.findElement(By.xpath("//div[@id='business_search']/div/div/div/div/p[4]/span"));
+				HighLight.highLightElement(webDriver, CAGECodesOpsS);
+				//Logout and login as an MPP Analyst.
+				webDriver.findElement(By.linkText("Logout")).click();
+				get_The_Row_From_Login_Data = 29;
+				LoginPageWithReference login_Data4 = new LoginPageWithReference(webDriver, get_The_Row_From_Login_Data);
+				login_Data4.Login_With_Reference();
+				//Search the business and verify the CAGE Codes.
+				webDriver.findElement(By.id("query")).sendKeys("791985859");
+				webDriver.findElement(By.xpath("//button")).click();
+				webDriver.findElement(By.linkText("Entity 90 Legal Business Name")).click();
+				Actual_Text = webDriver.findElement(By.xpath("//p[4]/b")).getText();
+				Expected_Text = "CAGE:";
+				assertEquals(Expected_Text, Actual_Text);
+				Actual_Text = webDriver.findElement(By.xpath("//div[@id='business_search']/div/div/div/div/p[4]/span")).getText();
+				Expected_Text = "Z495F, K70WB";
+				assertEquals(Expected_Text, Actual_Text);
+				WebElement CAGECodesMppA = webDriver.findElement(By.xpath("//div[@id='business_search']/div/div/div/div/p[4]/span"));
+				HighLight.highLightElement(webDriver, CAGECodesMppA);
+				return;	
+
+			} else {
 			Actual_Text = webDriver.findElement(By.cssSelector("h2 > a")).getText();
 			Expected_Text = "My business";
 			assertEquals(Expected_Text, Actual_Text);
@@ -41,7 +137,7 @@ import junit.framework.TestCase;
 			Expected_Text = "CAGE:";
 			assertEquals(Actual_Text, Expected_Text);
 			Actual_Text = webDriver.findElement(By.xpath("//p[3]/span")).getText();
-			Expected_Text = "4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R";
+			Expected_Text = "Z495F, K70WB";
 			assertEquals(Expected_Text, Actual_Text);
 			WebElement CAGECodes = webDriver.findElement(By.xpath("//p[3]/span"));
 			HighLight.highLightElement(webDriver, CAGECodes);
@@ -51,14 +147,14 @@ import junit.framework.TestCase;
 			LoginPageWithReference login_Data1 = new LoginPageWithReference(webDriver, get_The_Row_From_Login_Data);
 			login_Data1.Login_With_Reference();
 			//Search the business and verify the CAGE Codes.
-			webDriver.findElement(By.id("query")).sendKeys("137151292");
+			webDriver.findElement(By.id("query")).sendKeys("791985859");
 			webDriver.findElement(By.xpath("//button")).click();
-			webDriver.findElement(By.linkText("Entity 399 Legal Business Name")).click();
+			webDriver.findElement(By.linkText("Entity 90 Legal Business Name")).click();
 			Actual_Text = webDriver.findElement(By.xpath("//p[4]/b")).getText();
 			Expected_Text = "CAGE:";
 			assertEquals(Expected_Text, Actual_Text);
 			Actual_Text = webDriver.findElement(By.xpath("//div[@id='business_search']/div/div/div/div/p[4]/span")).getText();
-			Expected_Text = "4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R";
+			Expected_Text = "Z495F, K70WB";
 			assertEquals(Expected_Text, Actual_Text);
 			WebElement CAGECodesAP = webDriver.findElement(By.xpath("//div[@id='business_search']/div/div/div/div/p[4]/span"));
 			HighLight.highLightElement(webDriver, CAGECodesAP);
@@ -68,14 +164,14 @@ import junit.framework.TestCase;
 			LoginPageWithReference login_Data2 = new LoginPageWithReference(webDriver, get_The_Row_From_Login_Data);
 			login_Data2.Login_With_Reference();
 			//Search the business and verify the CAGE Codes.
-			webDriver.findElement(By.id("query")).sendKeys("137151292");
+			webDriver.findElement(By.id("query")).sendKeys("791985859");
 			webDriver.findElement(By.xpath("//button")).click();
-			webDriver.findElement(By.linkText("Entity 399 Legal Business Name")).click();
+			webDriver.findElement(By.linkText("Entity 90 Legal Business Name")).click();
 			Actual_Text = webDriver.findElement(By.xpath("//p[4]/b")).getText();
 			Expected_Text = "CAGE:";
 			assertEquals(Expected_Text, Actual_Text);
 			Actual_Text = webDriver.findElement(By.xpath("//div[@id='business_search']/div/div/div/div/p[4]/span")).getText();
-			Expected_Text = "4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R";
+			Expected_Text = "Z495F, K70WB";
 			assertEquals(Expected_Text, Actual_Text);
 			WebElement CAGECodes8aA = webDriver.findElement(By.xpath("//div[@id='business_search']/div/div/div/div/p[4]/span"));
 			HighLight.highLightElement(webDriver, CAGECodes8aA);
@@ -85,14 +181,14 @@ import junit.framework.TestCase;
 			LoginPageWithReference login_Data3 = new LoginPageWithReference(webDriver, get_The_Row_From_Login_Data);
 			login_Data3.Login_With_Reference();
 			//Search the business and verify the CAGE Codes.
-			webDriver.findElement(By.id("query")).sendKeys("137151292");
+			webDriver.findElement(By.id("query")).sendKeys("791985859");
 			webDriver.findElement(By.xpath("//button")).click();
-			webDriver.findElement(By.linkText("Entity 399 Legal Business Name")).click();
+			webDriver.findElement(By.linkText("Entity 90 Legal Business Name")).click();
 			Actual_Text = webDriver.findElement(By.xpath("//p[4]/b")).getText();
 			Expected_Text = "CAGE:";
 			assertEquals(Actual_Text, Expected_Text);
 			Actual_Text = webDriver.findElement(By.xpath("//div[@id='business_search']/div/div/div/div/p[4]/span")).getText();
-			Expected_Text = "4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R";
+			Expected_Text = "Z495F, K70WB";
 			assertEquals(Actual_Text, Expected_Text);
 			WebElement CAGECodesOpsS = webDriver.findElement(By.xpath("//div[@id='business_search']/div/div/div/div/p[4]/span"));
 			HighLight.highLightElement(webDriver, CAGECodesOpsS);
@@ -102,19 +198,19 @@ import junit.framework.TestCase;
 			LoginPageWithReference login_Data4 = new LoginPageWithReference(webDriver, get_The_Row_From_Login_Data);
 			login_Data4.Login_With_Reference();
 			//Search the business and verify the CAGE Codes.
-			webDriver.findElement(By.id("query")).sendKeys("137151292");
+			webDriver.findElement(By.id("query")).sendKeys("791985859");
 			webDriver.findElement(By.xpath("//button")).click();
-			webDriver.findElement(By.linkText("Entity 399 Legal Business Name")).click();
+			webDriver.findElement(By.linkText("Entity 90 Legal Business Name")).click();
 			Actual_Text = webDriver.findElement(By.xpath("//p[4]/b")).getText();
 			Expected_Text = "CAGE:";
 			assertEquals(Expected_Text, Actual_Text);
 			Actual_Text = webDriver.findElement(By.xpath("//div[@id='business_search']/div/div/div/div/p[4]/span")).getText();
-			Expected_Text = "4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R, 4RU2R";
+			Expected_Text = "Z495F, K70WB";
 			assertEquals(Expected_Text, Actual_Text);
 			WebElement CAGECodesMppA = webDriver.findElement(By.xpath("//div[@id='business_search']/div/div/div/div/p[4]/span"));
 			HighLight.highLightElement(webDriver, CAGECodesMppA);
 			return;	
-			
+		}
 		}
 
 		@After
