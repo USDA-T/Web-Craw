18156a2b5274dd959eb74f0b2f8ab658dbc30bd5
deepapatri <deepa.patri.ctr@telesishq.com>
2017-01-19 19:02:16 -0500

    All

--- src/test/java/gov/sba/utils/WorkflowPages/newMppUploadDocumentPage.java
+++ src/test/java/gov/sba/utils/WorkflowPages/newMppUploadDocumentPage.java
@@ -67,7 +67,7 @@ public class newMppUploadDocumentPage {
         robot.keyPress(KeyEvent.VK_ENTER);
         robot.keyRelease(KeyEvent.VK_ENTER);
         logger.info("Uploading a new document - Clicked on All Enters");                
-        Thread.sleep(2000);
+        Thread.sleep(5000);
         webDriver.findElement(By.cssSelector("#attach")).click();
     }
 
