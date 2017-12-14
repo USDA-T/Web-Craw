314590064563313164d70dbb0c7ffbe6787c3a6e
Montana33 <Montana33>
2017-12-14 16:03:24 -0500

    fomarting code

--- src/test/java/gov/usda/utils/integration/TestDataBaseExicution.java
+++ src/test/java/gov/usda/utils/integration/TestDataBaseExicution.java
@@ -1,46 +1,45 @@
 package gov.usda.utils.integration;
 
-	import org.apache.logging.log4j.LogManager;
-	import org.apache.logging.log4j.Logger;
-	import org.junit.After;
-	import org.junit.Before;
-	import org.junit.Test;
-	import org.openqa.selenium.WebDriver;
-	import gov.sba.automation.TestHelpers;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import gov.sba.automation.TestHelpers;
 import gov.usda.utils.integration.DataBaseExicutionPage;
 import junit.framework.TestCase;
 
-	public class TestDataBaseExicution extends TestCase {
-	  private static final Logger logger =
-	      LogManager.getLogger(TestDataBaseExicution.class.getName());
-	  private static WebDriver webDriver;
-	  int get_The_Row_From_Login_Data;
+public class TestDataBaseExicution extends TestCase {
+	private static final Logger logger = LogManager.getLogger(TestDataBaseExicution.class.getName());
+	private static WebDriver webDriver;
+	int get_The_Row_From_Login_Data;
 
-	  @Before
-	  public void setUp() throws Exception {
-	    webDriver = TestHelpers.getDefaultWebDriver();
-	    //webDriver.get(TestHelpers.getBaseUrl());
-	    // webDriver.manage().window().maximize();
-	    get_The_Row_From_Login_Data = 8;
-	  }
-
-	  @Test
-	  public void testDataBaseExicution() throws Exception {
-	      logger.info("Running querries in the db");
-	      // Login to Dashboard.
-	      //LoginPageWithReference login_Data =
-	          //new LoginPageWithReference(webDriver, get_The_Row_From_Login_Data);
-	      //login_Data.Login_With_Reference();
-	      //Database test
-	      DataBaseExicutionPage dataBaseExicution = new DataBaseExicutionPage(webDriver);
-	      dataBaseExicution.DataBaseExicution();
-	      
-	    logger.info("Success");
-
-	  }
-
-	  @After
-	  public void tearDown() throws Exception {
-	    webDriver.close();
-	  }
+	@Before
+	public void setUp() throws Exception {
+		webDriver = TestHelpers.getDefaultWebDriver();
+		// webDriver.get(TestHelpers.getBaseUrl());
+		// webDriver.manage().window().maximize();
+		get_The_Row_From_Login_Data = 8;
 	}
+
+	@Test
+	public void testDataBaseExicution() throws Exception {
+		logger.info("Running querries in the db");
+		// Login to Dashboard.
+		// LoginPageWithReference login_Data =
+		// new LoginPageWithReference(webDriver, get_The_Row_From_Login_Data);
+		// login_Data.Login_With_Reference();
+		// Database test
+		DataBaseExicutionPage dataBaseExicution = new DataBaseExicutionPage(webDriver);
+		dataBaseExicution.DataBaseExicution();
+
+		logger.info("Success");
+
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		webDriver.close();
+	}
+}
--- src/test/java/gov/usda/utils/integration/TestSearchPage.java
+++ src/test/java/gov/usda/utils/integration/TestSearchPage.java
@@ -21,48 +21,48 @@ import gov.usda.utils.integration.LoginHelpers;
 import junit.framework.TestCase;
 
 @Ignore
-@Category({gov.usda.utils.integration.StableTests.class})
+@Category({ gov.usda.utils.integration.StableTests.class })
 // @Category(StableTests.class)
 public class TestSearchPage extends TestCase {
-  private static final Logger logger = LogManager.getLogger(TestSearchPage.class.getName());
-  private static WebDriver webDriver;
+	private static final Logger logger = LogManager.getLogger(TestSearchPage.class.getName());
+	private static WebDriver webDriver;
 
-  @BeforeClass
-  public static void setUpBeforeClass() throws Exception {}
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
 
-  @AfterClass
-  public static void tearDownAfterClass() throws Exception {}
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
 
-  @Before
-  public void setUp() throws Exception {
-    webDriver = TestHelpers.getDefaultWebDriver();
+	@Before
+	public void setUp() throws Exception {
+		webDriver = TestHelpers.getDefaultWebDriver();
 
-    webDriver.get(TestHelpers.getBaseUrl());
-    // Get the login based on the environment under test (e.g.
-    // 'development', 'qa', 'staging')
-    logger.info("FYI: your environment under test:" + System.getProperty(Constants.TEST_ENV));
-  }
+		webDriver.get(TestHelpers.getBaseUrl());
+		// Get the login based on the environment under test (e.g.
+		// 'development', 'qa', 'staging')
+		logger.info("FYI: your environment under test:" + System.getProperty(Constants.TEST_ENV));
+	}
 
-  @After
-  public void tearDown() throws Exception {
-    webDriver.quit();
-  }
+	@After
+	public void tearDown() throws Exception {
+		webDriver.quit();
+	}
 
-  @Test()
-  // @Category(StableTests.class)
-  public void testMainLogic() throws Exception {
-    logger.debug("Using test login   : " + LoginHelpers.getLoginData().getEmail());
-    logger.debug("Using test password: " + LoginHelpers.getLoginData().getPassword());
-    click_Element(webDriver, "SBA_Login_Button");
+	@Test()
+	// @Category(StableTests.class)
+	public void testMainLogic() throws Exception {
+		logger.debug("Using test login   : " + LoginHelpers.getLoginData().getEmail());
+		logger.debug("Using test password: " + LoginHelpers.getLoginData().getPassword());
+		click_Element(webDriver, "SBA_Login_Button");
 
-    CommonApplicationMethods.setText_Element(webDriver, "SBA_Login_Email",
-        LoginHelpers.getLoginData().getEmail());
-    CommonApplicationMethods.setText_Element(webDriver, "SBA_Login_Pwd",
-        LoginHelpers.getLoginData().getPassword());
+		CommonApplicationMethods.setText_Element(webDriver, "SBA_Login_Email", LoginHelpers.getLoginData().getEmail());
+		CommonApplicationMethods.setText_Element(webDriver, "SBA_Login_Pwd", LoginHelpers.getLoginData().getPassword());
 
-    click_Element(webDriver, "OppSup_Dashboard_Business_Signin");
-    Thread.sleep(1500); // Deepa Sleep needed here
-    org.junit.Assert.assertTrue(webDriver.getCurrentUrl().contains("dashboard"));
-  }
+		click_Element(webDriver, "OppSup_Dashboard_Business_Signin");
+		Thread.sleep(1500); // Deepa Sleep needed here
+		org.junit.Assert.assertTrue(webDriver.getCurrentUrl().contains("dashboard"));
+	}
 
 }
